3923e198907295e440cacffb6facf12b74bf4c63
liyongli@star-v.com.cn <liyongli@star-v.com.cn>
2019-04-25 17:30:41 +0800

    添加

---
+++ precache-manifest.25c278db99e01c8fad9c1b3ce2bd3d68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "/js/about.69260e02.js"
+  },
+  {
+    "revision": "b63b1f6a99f80d4d2b5f",
+    "url": "/css/app.cbd0c9e4.css"
+  },
+  {
+    "revision": "b63b1f6a99f80d4d2b5f",
+    "url": "/js/app.a494e0ea.js"
+  },
+  {
+    "revision": "d4b8ba00e71909a0adcd",
+    "url": "/js/chunk-vendors.6085e851.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "04201314b79eb1829d62615c2d0c695c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
